bc04b92ba2c8c6130474167437359b18f45c608a
Kanokphon <624259001@webmail.npru.ac.th>
2021-10-19 17:47:23 +0700

    Login

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,11 +8,19 @@
     <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
+<div class="navbar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="/register">ลงทะเบียน</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="">Search</a></li>
+            </ul>
+        </div>
     <form class="box" action="" method="POST" >
        <h1>Login</h1>
         <input type="text" name="" placeholder="เลขบัตรประชาชน">
         <input type="password" name="" placeholder="รหัสผ่าน">
-        <input type="submit" name="" value="">
+        <input type="submit" name="" value="เข้าสู่ระบบ">
     </form>
     
 </body>
